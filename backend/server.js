8e361fee06801aa97063f3a10d796cfe6b92fd5a
mohit <mohitypatil2907@gmail.com>
2026-04-03 21:15:03 +0530

    week 2 work

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const pool = require("./src/config/db");
 
 const app = express();
 
@@ -13,4 +14,20 @@ app.get("/", (req, res) => {
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({
+      success: true,
+      time: result.rows[0],
+    });
+  } catch (error) {
+    console.error("DB Error:", error);
+    res.status(500).json({
+      success: false,
+      message: "Database connection failed",
+    });
+  }
 });
